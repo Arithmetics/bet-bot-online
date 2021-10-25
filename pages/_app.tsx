0907d9430e7dce36a6fed5d4b2868a4d495d82b9
Brock Tillotson <brock.m.tillotson@gmail.com>
2021-10-24 21:29:11 -0700

    first save

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <GeistProvider themeType='dark'>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </GeistProvider> 
 }
-export default MyApp
+
+export default MyApp;
