c780f3a52b7cc38b911bb3701c65f9a380340ce9
Brock Tillotson <brock.m.tillotson@gmail.com>
2022-02-21 16:50:59 -0800

    discord work

---
+++ backend/src/botInformation.ts
@@ -0,0 +1,12 @@
+export const ownerIds: Record<string, string> = {
+  "111938054297505792": "jerms",
+  "507719783014465537": "kerm",
+  "306086225016782849": "brock",
+  "676882375929757748": "dagr",
+  "605411339309023255": "trox",
+  "677695491437363257": "jhi",
+};
+
+export const channelIds: Record<string, string> = {
+  "675574196268564525": "bets",
+};
