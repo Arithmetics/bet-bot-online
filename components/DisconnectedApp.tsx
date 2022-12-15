273cd035f464045fb0e1ab4481ecc58db158ac97
Arithmetics <brock.m.tillotson@gmail.com>
2022-12-14 22:36:09 -0800

    refresh button

--- components/DisconnectedApp.tsx
+++ components/DisconnectedApp.tsx
@@ -1,9 +1,10 @@
-import {
-  Grid, Text
-} from "@geist-ui/react";
+import { Button, Grid, Text } from "@geist-ui/react";
 import AlertTriangle from "@geist-ui/react-icons/alertTriangle";
 
 export default function DisconnectedApp(): JSX.Element {
+  const refresh = () => {
+    window.location.reload();
+  };
   return (
     <Grid.Container
       direction="column"
@@ -18,6 +19,19 @@ export default function DisconnectedApp(): JSX.Element {
       </Grid>
       <Grid xs={12}>
         <AlertTriangle color="red" size={36} />
+      </Grid>
+      <Grid xs={24}>
+        <Button
+          type="error"
+          ghost
+          scale={0.5}
+          onClick={refresh}
+          style={{
+            marginTop: "16px",
+          }}
+        >
+          Reload
+        </Button>
       </Grid>
     </Grid.Container>
   );
