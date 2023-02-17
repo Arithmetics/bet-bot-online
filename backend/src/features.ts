96f6f8067ac1af9b26af13febe8e8b50dbc45d28
Arithmetics <brock.m.tillotson@gmail.com>
2023-02-16 18:35:31 -0800

    time guards totals

--- backend/src/features.ts
+++ backend/src/features.ts
@@ -15,4 +15,7 @@ export const TOTAL_BET_THRESHOLD = 4.999;
 export const ATS_LOW_MINUTE = 12;
 export const ATS_HIGH_MINUTE = 36;
 
+export const TOTAL_LOW_MINUTE = 6;
+export const TOTAL_HIGH_MINUTE = 42;
+
 export default featureFlags;
